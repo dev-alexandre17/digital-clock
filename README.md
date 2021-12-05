3d71a0827c1dbac12fbb695613f209edc243fb61
dev-alexandre17 <alexandretec77@gmail.com>
2021-12-05 20:39:17 -0300

    Modificando arquivo

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1> Relógio Digital </h1> 
+
+<p> Projeto inspirado em um curso de Python realizado na Udemy. </p>
+
+<br>
+
+<img src="screenshot-projeto\imagem-fundo-relogio.png">
+
--- clock.py
+++ clock.py
@@ -1,4 +1,4 @@
-# Bibliotecas para interface
+# Bibliotecas para interface gráfica
 
 from tkinter import *
 from tkinter import ttk
@@ -6,12 +6,7 @@ from tkinter import font
 import time
 import datetime
 
-# Fechar aplicação
-
-def quit(*argx):
-    root.destroy()
-
-# Retorna o tempo real
+# Tempo do relógio
 
 def clock_time():
     time = datetime.datetime.now()
@@ -23,27 +18,32 @@ def clock_time():
 
 root = Tk()
 
-# Atributos em relação a resolução da tela
+# Título do programa
 
-root.attributes("-fullscree", False)
+root.title(f'Digital clock')
 
-# Configuração de cor do fundo do relógio
+# Configuração de imagem de fundo
 
-root.configure(background='light blue')
+photo = PhotoImage(file="imagem\lo-fi.png")
+label1 = Label(root, image=photo) 
+label1.place(x = -5, y = 0) 
 
-# Configuração para fechar a aplicação (quit)
+# Definição de altura e largura da janela
 
-root.bind("x", quit)
+root.minsize(450, 350)
+root.maxsize(450, 350)
 
 # Configuração de milisegundos no relógio (clock_time)
 
 root.after(1000, clock_time)
 
-# Fonte, tipo, tamanho e estilo
+# Conigurações gerais
 
-fnt = font.Font(family='Helvetica', size=120, weight='bold')
+fnt = font.Font(family='Arial', size=40, weight='bold')
 txt = StringVar()
-lbl = ttk.Label(root, textvariable=txt, font=fnt, foreground="black", background="light blue")
-lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
+lbl = ttk.Label(root, textvariable=txt, font=fnt, foreground='black', background='white')
+lbl.place(relx=0.5, rely=0.17, anchor=CENTER)
+
+# Iniciar aplicação
 
 root.mainloop()
